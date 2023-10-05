29d8864125c7b88e0485c950e330a4541e278665
Yonis7 <yonisaden7@gmail.com>
2023-10-05 02:24:23 -0400

    Added onClick and onInput event handling.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   newMemberName = '';
   members: string[] = [];
+
+  onInput(member: string) {
+    this.newMemberName = member;
+    console.log(this.newMemberName);
+  }
+
+  // This method is called when the user clicks the "Add Member" button. It adds the new member to the members array by calling the push() method on the array. The push() method adds the new member to the end of the array. The new member is the value of the newMemberName property. 
+  addMember() {
+    this.members.push(this.newMemberName);
+    console.log(this.members);
+  }
 }
