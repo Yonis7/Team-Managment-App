949240351bdee0d5e4f6078859d15ab3965ed658
Yonis7 <yonisaden7@gmail.com>
2023-10-05 02:56:44 -0400

    Used atribute directives and error handling.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,23 @@ import { Component } from '@angular/core';
 export class AppComponent {
   newMemberName = '';
   members: string[] = [];
+  errorMessage = '';
 
   onInput(member: string) {
     this.newMemberName = member;
-    console.log(this.newMemberName);
+    // console.log(this.newMemberName);
   }
 
-  // This method is called when the user clicks the "Add Member" button. It adds the new member to the members array by calling the push() method on the array. The push() method adds the new member to the end of the array. The new member is the value of the newMemberName property. 
+  // This method is called when the user clicks the "Add Member" button. It adds the new member to the members array by calling the push() method on the array. The push() method adds the new member to the end of the array. The new member is the value of the newMemberName property.
   addMember() {
+    if (!this.newMemberName) {
+      this.errorMessage = 'Please enter a name.';
+      return;
+    }
+
+    this.errorMessage = '';
     this.members.push(this.newMemberName);
-    console.log(this.members);
+    this.newMemberName = '';
+    // console.log(this.members);
   }
 }
